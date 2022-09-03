6ce319cd4867ae5abbd2ae41b7b9ca9298772edb
Marco <marcoarcha@hotmail.com>
2022-09-03 07:00:37 -0600

    Global state for register alerts

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useState } from 'react'
+import { useAppContext } from '../context/appContext'
 
 const Alert = () => {
-
+  const {alertType, alertText} = useAppContext()
   return (
-    <div className='alert alert-danger'>Alert de prueba</div>
+    <div className={`alert alert-${alertType}`}>{alertText}</div>
   )
 }
 
